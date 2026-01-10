4b505d991159564fc47cf0d0f21bed44d7f2ab6f
sebastianput <43670106+sebastianput@users.noreply.github.com>
2026-01-10 22:29:07 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+<HashRouter>
+  <App />
+</HashRouter>

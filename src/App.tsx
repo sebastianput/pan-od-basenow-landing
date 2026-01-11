214cf257a71ef42f6b629a71bcfa7980a26c7912
sebastianput <43670106+sebastianput@users.noreply.github.com>
2026-01-11 20:49:49 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,15 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      {/* GitHub Pages requires basename = repo name */}
+      <BrowserRouter basename="/pan-od-basenow-landing">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
